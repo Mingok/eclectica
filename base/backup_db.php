64b57937c8b7179167eda9d8c9e2ac3339c543a0
Emanuel Kling <emanuelkling@hotmail.com>
2014-05-27 18:29:30 -0300

    Se completo el sistema de backup

---
+++ base/backup_db.php
@@ -0,0 +1,14 @@
+<?php
+require_once (__DIR__.'\\manejoMySQL.php');
+$objManejoMySQL = new manejoMySQL();
+$dbhost = $objManejoMySQL->host;
+$dbuser = $objManejoMySQL->username;
+$dbpass = $objManejoMySQL->password;
+$dbname = $objManejoMySQL->database;
+
+$backup_file = 'db_' . $dbname . '_' . date("l") . '.sql';
+if (file_exists($backup_file)) {
+    unlink($backup_file);
+} 
+$command = "C:/wamp/bin/mysql/mysql5.6.17/bin/mysqldump.exe --opt --host=$dbhost --user=$dbuser --password= $dbname > $backup_file";
+exec($command);
