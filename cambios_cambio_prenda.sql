d0d004cb1e4bcffcfc066bb57043549ca262a981
Mingok <emanuelkling@hotmail.com>
2014-08-07 22:48:40 -0300

    no message

--- cambios_cambio_prenda.sql
+++ cambios_cambio_prenda.sql
@@ -2,3 +2,5 @@ ALTER TABLE `eclectica`.`venta_renglon`
   ADD COLUMN `estado` VARCHAR(1) DEFAULT 'V'   NOT NULL AFTER `precioVendido`;
 ALTER TABLE `eclectica`.`venta`   
   CHANGE `estado` `estado` VARCHAR(1) CHARSET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `eclectica`.`entrega`   
+  ADD COLUMN `tipo` VARCHAR(1) DEFAULT 'E'   NOT NULL AFTER `inicial`;
